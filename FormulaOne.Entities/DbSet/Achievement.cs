de6a5ad1e3193ab5b60f482800f72a06bc2d5b01
Aslam Shaikh <beingaslam@outlook.in>
2024-02-12 16:13:48 +0530

    Clean up files, Added Entities & Packages

---
+++ FormulaOne.Entities/DbSet/Achievement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaOne.Entities.DbSet
+{
+    public class Achievement
+    {
+        public int RaceWins { get; set; }
+        public int PolePosition { get; set; }
+        public int FastestLap { get; set; }
+        public int WorldChapionship { get; set; }
+        public Guid DriverId { get; set; }
+        public virtual Driver? Driver { get; set; }
+    }
+}
